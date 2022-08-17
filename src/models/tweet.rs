60fd493eadc11d34adfe3159e6a75d4672fbc2c8
MegaHotel <megahotel1000@gmail.com>
2022-08-17 13:43:49 -0400

    all model fields pub

--- src/models/tweet.rs
+++ src/models/tweet.rs
@@ -6,33 +6,33 @@ use serde::{Deserialize, Serialize};
 pub struct Tweet {
     #[serde(deserialize_with = "str_to_datetime")]
     #[serde(serialize_with = "datetime_to_str")]
-    created_at: DateTime<Utc>,
+    pub created_at: DateTime<Utc>,
     #[serde(rename = "id_str")]
     #[serde(deserialize_with = "int_from_str_id")]
-    id: u64,
+    pub id: u64,
     #[serde(rename = "full_text")]
-    text: String,
+    pub text: String,
     #[serde(default)]
-    tweet_url: Option<String>,
+    pub tweet_url: Option<String>,
     #[serde(rename = "entities")]
     #[serde(deserialize_with = "from_entities")]
-    entities: Entities,
+    pub entities: Entities,
     #[serde(rename = "in_reply_to_status_id_str")]
     #[serde(deserialize_with = "int_opt_from_str_id")]
-    in_reply_to_tweet_id: Option<u64>,
+    pub in_reply_to_tweet_id: Option<u64>,
     #[serde(rename = "in_reply_to_user_id_str")]
     #[serde(deserialize_with = "int_opt_from_str_id")]
-    in_reply_to_user_id: Option<u64>,
+    pub in_reply_to_user_id: Option<u64>,
     #[serde(rename = "source")]
     #[serde(deserialize_with = "platform_enum_from_source")]
-    platform: Platform,
-    favorite_count: usize,
-    quote_count: usize,
-    reply_count: usize,
-    retweet_count: usize,
+    pub platform: Platform,
+    pub favorite_count: usize,
+    pub quote_count: usize,
+    pub reply_count: usize,
+    pub retweet_count: usize,
     #[serde(rename = "lang")]
-    language: String,
-    card: Option<Card>,
+    pub language: String,
+    pub card: Option<Card>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -49,10 +49,10 @@ pub struct Media {
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Card {
-    url: String,
+    pub url: String,
     #[serde(rename = "binding_values")]
     #[serde(deserialize_with = "image_url_from_card")]
-    image_url: Option<String>,
+    pub image_url: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
--- src/models/user.rs
+++ src/models/user.rs
@@ -6,23 +6,23 @@ use serde::{Deserialize, Serialize};
 pub struct User {
     #[serde(deserialize_with = "str_to_datetime")]
     #[serde(serialize_with = "datetime_to_str")]
-    created_at: DateTime<Utc>,
+    pub created_at: DateTime<Utc>,
     #[serde(rename = "id_str")]
     #[serde(deserialize_with = "int_from_str_id")]
-    id: u64,
-    name: String,
-    screen_name: String,
-    description: String,
-    location: String,
-    statuses_count: u64,
-    followers_count: u64,
-    friends_count: u64,
-    favourites_count: u64,
-    listed_count: u64,
-    media_count: u64,
-    profile_image_url_https: String,
-    verified: bool,
+    pub id: u64,
+    pub name: String,
+    pub screen_name: String,
+    pub description: String,
+    pub location: String,
+    pub statuses_count: u64,
+    pub followers_count: u64,
+    pub friends_count: u64,
+    pub favourites_count: u64,
+    pub listed_count: u64,
+    pub media_count: u64,
+    pub profile_image_url_https: String,
+    pub verified: bool,
     #[serde(rename = "profile_image_url_https")]
     #[serde(default)]
-    image: Option<String>,
+    pub image: Option<String>,
 }
